b50ec62f3179b0272117b674b9bbdb74ffc2a4cf
Drealler <ra.lencar@hotmail.com>
2021-09-11 03:57:37 -0300

    Added cors and helmet

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
-import * as dotenv from 'dotenv';
+import cors from "cors";
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from "express";
+import helmet from "helmet";
 import MongooseConnection from "./src/connection/Mongoose.connection";
 import v1 from "./src/routes/v1";
 
@@ -13,12 +15,14 @@ const { AMBIENT, PORT } = process.env;
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.info(req.method, req.originalUrl);
   next();
-}
+};
 
+app.use(helmet());
+app.use(cors());
 app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/v1', v1);
+app.use("/v1", v1);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
