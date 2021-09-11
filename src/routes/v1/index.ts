ad3d10d2ed2add3b05c9f1a67367bde912dde39b
TsunekiR <tsunekiroberto@gmail.com>
2021-09-11 00:47:01 -0300

    first attempt on category testing

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import Category from "./Category";
 import Order from "./Order";
 import Product from "./Product";
 import Ingredient from "./Ingredient ";
 
 const router = Router();
 
+router.use("/category", Category);
 router.use("/orders", Order);
 router.use("/products", Product);
 router.use("/ingredients", Ingredient);
--- test/category.model.test.ts
+++ test/category.model.test.ts
@@ -1,35 +1,35 @@
 import { connect, Connection, connection, models } from "mongoose";
 import ProductCategoryModel from "../src/routes/v1/Category/model";
+import request from 'supertest';
+import express from 'express';
+import { app } from "../app";
 
 const dbtype = process.env.dbtype ? "mongo" : "localhost";
 
 let conn:any;
 
 beforeAll(async () => {
-  conn = await connect("mongodb://localhost:27017/testUser");
-  console.log(conn);
+  conn = await connect("mongodb://localhost:27017/testCategory");
 });
 
-describe("Category Model", () => {
-  it("has name attributes", () => {
-    let expectedKeys = ["name"];
-    let keys = Object.keys(ProductCategoryModel.schema.paths);
-    let categoryAttributes = [keys[0]];
-    expect(categoryAttributes).toStrictEqual(expectedKeys);
-  });
+test("POST /category response code", async () =>{
+  const response = await request(app).post("/v1/category").send({
+    name: "Vegetais"
+  })
+  expect(response.statusCode).toBe(200)
 });
 
-it("should create a new category", async () => {
-  try {
-    const Category = new ProductCategoryModel({
-      name: "Vegetais",
-    });
-    console.log(Category);
-    let result = await Category.save();
-    console.log(result);
-    expect(result.name).toEqual(Category.name);
-  } catch (err) {
-  }
+test("POST /category response content", async () =>{
+  const response = await request(app).post("/v1/category").send({
+    name: "Vegetais"
+  })
+  expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+});
+
+test("POST /category response no content", async () =>{
+  const response = await request(app).post("/v1/category").send({
+  })
+  expect(response.statusCode).toBe(400)
 });
 
 afterAll(async () => {
