16e81228ef197d2b8dd4efaaa8ed53838f87a709
Drealler <ra.lencar@hotmail.com>
2021-10-06 18:33:46 -0300

    Added new scripts

--- src/connection/Mongoose.connection.ts
+++ src/connection/Mongoose.connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 let connectionString = (process.env.MONGOOSE_URI as string);
 
-if (process.env.NODE_ENV == "test") {
+if (process.env.NODE_ENV === "test") {
   connectionString = "mongodb://localhost:27017/testDb";
 }
 
